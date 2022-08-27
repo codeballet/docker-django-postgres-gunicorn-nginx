0a2ba139a908b1381aab1a795db6158e56a3a373
johan <stjernholm@msn.com>
2022-08-27 12:38:42 +0800

    Feature: Nginx working with hello app

---
+++ app/hello/static/hello/index.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const h2 = document.createElement('h2');
+    h2.innerHTML = "Created in JavaScript!";
+    document.querySelector('body').append(h2);
+})
--- app/hello_django/settings.py
+++ app/hello_django/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 # Application definition
 
 INSTALLED_APPS = [
+    'hello',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -73,13 +74,6 @@ WSGI_APPLICATION = 'hello_django.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 DATABASES = {
     "default": {
@@ -128,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
